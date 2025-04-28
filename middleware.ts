0f19c185bd72373af26b84957a9a248761e60fa1
John Kayrous <116210895+JohnKayrouss@users.noreply.github.com>
2025-04-28 10:55:43 -0400

    debug-clerk

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,21 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// import { NextResponse } from "next/server";
-// const isPublicRoute = createRouteMatcher([
-// 	"/",
-// 	"/about",
-// 	"/services",
-// 	"/houses/(.*)",
-// 	"/houses",
-// 	"/admin/(.*)",
-// ]);
+import { NextResponse } from "next/server";
+const isPublicRoute = createRouteMatcher([
+	"/",
+	"/about",
+	"/services",
+	"/houses/(.*)",
+	"/houses",
+	"/admin/(.*)",
+]);
 
-// export default clerkMiddleware(async (auth, req) => {
-// 	if (!isPublicRoute(req)) {
-// 		auth.protect();
-// 	}
-// });
-
-// export const config = {
-// 	matcher: [
-// 		// Skip Next.js internals and all static files, unless found in search params
-// 		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-// 		// Always run for API routes
-// 		"/(api|trpc)(.*)",
-// 	],
-// };
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+	if (!isPublicRoute(req)) {
+		auth.protect();
+	}
+});
 
 export const config = {
-	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-		// Always run for API routes
-		"/(api|trpc)(.*)",
-	],
+	matcher: ["/((?!_next|.*\\..*).*)"], // matches everything except _next/ and files like .png, .jpg
 };
