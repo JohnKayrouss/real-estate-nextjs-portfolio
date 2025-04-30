8007b4242e84fc3a58657e268e7bf0540b539e52
John Kayrous <116210895+JohnKayrouss@users.noreply.github.com>
2025-04-29 20:53:10 -0400

    cloudinary-v2-update

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,7 +1,7 @@
 "use server";
-import cloudnary from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
-cloudnary.v2.config({
+cloudinary.config({
 	cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
@@ -9,7 +9,7 @@ cloudnary.v2.config({
 
 export const removeImageFromCloudinary = async (publicId: string) => {
 	try {
-		const res = await cloudnary.v2.uploader.destroy(publicId);
+		const res = await cloudinary.uploader.destroy(publicId);
 	} catch (error) {
 		return error;
 	}
